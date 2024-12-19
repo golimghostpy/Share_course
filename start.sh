910205e3e63d95ebb51491f06014aaed07f223e1
golimghostpy <98097829+golimghostpy@users.noreply.github.com>
2024-12-19 20:53:11 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 echo "Сейчас потребуется пароль для установки библиотеки виртуальных сред python"
+sudo apt update
 
-sudo apt install pyhton3-venv
-python3 -m venv myenv
+sudo apt install -y python3 python3-venv
 
-source myenv/bin/activate
+VENV_NAME="myenv"
+
+if [ ! -d "$VENV_NAME" ]; then
+    echo "Создание виртуальной среды..."
+    python3 -m venv $VENV_NAME
+fi
+
+source $VENV_NAME/bin/activate
 
 pip install PyQt5
 
-python3 PeopleAndPlaces.py
+python your_script.py
+
+deactivate
