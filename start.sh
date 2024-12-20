ff3d4503031d41a2804370286ef627568e3fc387
golimghostpy <98097829+golimghostpy@users.noreply.github.com>
2024-12-20 13:12:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,18 +8,12 @@ sudo apt install -y python3 python3-venv
 VENV_NAME="myenv"
 
 if [ ! -d "$VENV_NAME" ]; then
-    echo "Создание виртуальной среды..."
-    python3 -m venv $VENV_NAME
+echo "Создание виртуальной среды..."
+python3 -m venv $VENV_NAME
 fi
 
 source $VENV_NAME/bin/activate
 
 pip install PyQt5
 
-# Проверка установки PyQt5
-if ! pip show PyQt5 &> /dev/null; then
-    echo "PyQt5 не установлен. Попробуйте установить его через системный пакетный менеджер."
-    sudo apt-get install python3-pyqt5
-else
-    python PeopleAndPlaces.py
-fi
+python PeopleAndPlaces.py
